d60ef8843244c87f281700c322ca8ffba8549f6c
Junaid Anjum <junaidanjum@live.com>
2024-11-28 19:52:22 +0530

    4 chemex methods added.

--- src/lib/coffee-methods.ts
+++ src/lib/coffee-methods.ts
@@ -171,4 +171,154 @@ export const brewingMethods: BrewingMethod[] = [
       },
     ],
   },
+  {
+    id: "chemex",
+    name: "Chemex",
+    description: "A pour-over style glass coffeemaker.",
+    techniques: [
+      {
+        id: "301",
+        name: "James Hoffmann Chemex recipe",
+        description:
+          "Chemex recipe by James Hoffmann, winner of World Barista Championship, YouTuber, coffee consultant, and author. James recommends almost the same technique as for V60.",
+        ratio: 500 / 30,
+        defaultCoffeeAmount: 30,
+        steps: (coffeeAmount, waterAmount) => [
+          {
+            instruction:
+              "Grind the beans, boil the water, prepare Chemex with rinsed filter, add grounds, zero the scale.",
+            duration: 10,
+          },
+          {
+            instruction: `Add ${2 * coffeeAmount}-${
+              3 * coffeeAmount
+            }g of water.`,
+            duration: 10,
+          },
+          {
+            instruction: "Stir to wet all the grounds, let it bloom.",
+            duration: 35,
+          },
+          {
+            instruction: `Add water in a circular motion until you reach ${
+              0.6 * waterAmount
+            }g.`,
+            duration: 30,
+          },
+          {
+            instruction: `Continue to add water until you reach ${waterAmount}g.`,
+            duration: 30,
+          },
+          {
+            instruction: "Stir clockwise and counter-clockwise with a spoon.",
+            duration: 15,
+          },
+          { instruction: "Give the Chemex a little shake.", duration: 10 },
+          { instruction: "Wait until drawdown completes.", duration: 120 },
+        ],
+      },
+      {
+        id: "302",
+        name: "Chemex recipe by Caffeine Fiend",
+        description: "Chemex recipe by Caffeine Fiend.",
+        ratio: 480 / 32,
+        defaultCoffeeAmount: 32,
+        steps: (coffeeAmount, waterAmount) => [
+          {
+            instruction:
+              "Grind coffee, heat water, prepare Chemex with rinsed filter, add grounds.",
+            duration: 10,
+          },
+          {
+            instruction: `Pre-wet with ${
+              2 * coffeeAmount
+            }g of water, pour in spiral pattern.`,
+            duration: 15,
+          },
+          { instruction: "Let the coffee bloom.", duration: 45 },
+          {
+            instruction:
+              "Pour in concentric circles until the water reaches the top of the Chemex.",
+            duration: 20,
+          },
+          {
+            instruction: "Wait for grounds to return to original level.",
+            duration: 30,
+          },
+          {
+            instruction: `Repeat full immersion pours until you reach ${waterAmount}g. Wait for drawdown.`,
+            duration: 130,
+          },
+        ],
+      },
+      {
+        id: "303",
+        name: "Chemex recipe by Epicurious",
+        description: "Chemex recipe by Epicurious.",
+        ratio: 700 / 42,
+        defaultCoffeeAmount: 42,
+        steps: (coffeeAmount, waterAmount) => [
+          {
+            instruction:
+              "Grind coffee, heat water, prepare Chemex with rinsed filter, add grounds.",
+            duration: 10,
+          },
+          {
+            instruction: `Pre-wet with ${
+              2 * coffeeAmount
+            }g of water to saturate the grounds.`,
+            duration: 15,
+          },
+          { instruction: "Let the coffee bloom.", duration: 30 },
+          {
+            instruction: `Add up to ${
+              0.33 * waterAmount
+            }g of water in circular motion.`,
+            duration: 15,
+          },
+          { instruction: "Wait.", duration: 40 },
+          {
+            instruction: `Add water until scale reads ${0.64 * waterAmount}g.`,
+            duration: 20,
+          },
+          { instruction: "Wait for 2 minutes.", duration: 120 },
+          {
+            instruction: `Add water to ${waterAmount}g. Wait for drawdown.`,
+            duration: 60,
+          },
+        ],
+      },
+      {
+        id: "304",
+        name: "Scott Rao Chemex recipe",
+        description:
+          "Chemex recipe by Scott Rao, a coffee expert and author specializing in barista training, coffee roasting, and brewing.",
+        ratio: 510 / 30,
+        defaultCoffeeAmount: 30,
+        steps: (coffeeAmount, waterAmount) => [
+          {
+            instruction:
+              "Grind coffee, heat water, prepare Chemex with rinsed filter, add grounds.",
+            duration: 10,
+          },
+          {
+            instruction: `Pre-wet with ${1.5 * coffeeAmount}g of water.`,
+            duration: 10,
+          },
+          {
+            instruction: "Gently dig in the coffee bed with a small spoon.",
+            duration: 25,
+          },
+          { instruction: "Wait for bloom.", duration: 5 },
+          {
+            instruction: `Pour until ${waterAmount}g in circular patterns.`,
+            duration: 30,
+          },
+          { instruction: "Light stir at slurry edge.", duration: 10 },
+          { instruction: "Move Chemex in circles.", duration: 10 },
+          { instruction: "Wait for drawdown.", duration: 60 },
+        ],
+      },
+    ],
+  },
 ];
