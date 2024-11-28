94a7fe66f1e10e96d4f98791d8b6955fd13c46ec
Junaid Anjum <junaidanjum@live.com>
2024-11-28 18:40:36 +0530

    metadata; favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,10 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Webrew",
-  description: "Webrew",
+  description:
+    "A no-frills brewing timer app, built purely for the love of coffee.",
 };
+
 
 import { Geist } from "next/font/google";
 
@@ -18,6 +20,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="A no-frills brewing timer app, built purely for the love of coffee."
+        />
+        <meta name="author" content="Junaid Anjum" />
+        <meta name="og:title" content="Webrew" />
+        <meta
+          name="og:description"
+          content="A no-frills brewing timer app, built purely for the love of coffee."
+        />
+        <meta name="og:image" content="/icon.png" />
+      </head>
       <body className={`${font.className} antialiased`}>{children}</body>
     </html>
   );
