e969c32f031c7e9091f56f27ddbeb11fdd08fe73
Junaid Anjum <junaidanjum@live.com>
2024-11-29 23:46:16 +0530

    save recently used method to local storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,58 @@
-"use client";
+"use client"
 
-import { useState } from "react";
-import {
-  brewingMethods,
-  BrewingMethod,
-  BrewingTechnique,
-} from "@/lib/coffee-methods";
-import MethodSelection from "@/components/method-selection";
-import BrewingGuide from "@/components/brewing-guide";
-import CoffeeAmountInput from "@/components/coffee-amount-input";
-import { Coffee } from "lucide-react";
-import Footer from "@/components/footer";
+import { useState } from "react"
+import { brewingMethods, BrewingMethod, BrewingTechnique } from "@/lib/coffee-methods"
+import MethodSelection from "@/components/method-selection"
+import BrewingGuide from "@/components/brewing-guide"
+import CoffeeAmountInput from "@/components/coffee-amount-input"
+import { Coffee } from "lucide-react"
+import Footer from "@/components/footer"
 
 export default function CoffeeBrewingApp() {
-  const [selectedMethod, setSelectedMethod] = useState<BrewingMethod>(
-    brewingMethods[0]
-  );
-  const [selectedTechnique, setSelectedTechnique] = useState<BrewingTechnique>(
-    brewingMethods[0].techniques[0]
-  );
-  const [coffeeAmount, setCoffeeAmount] = useState(
-    selectedTechnique.defaultCoffeeAmount
-  );
-  const [waterAmount, setWaterAmount] = useState(
-    selectedTechnique.ratio * selectedTechnique.defaultCoffeeAmount
-  );
+	const initialMethod = localStorage && localStorage.getItem("selectedMethod") ? brewingMethods.find(m => m.id === localStorage.getItem("selectedMethod")) : brewingMethods[0]
+	const [selectedMethod, setSelectedMethod] = useState<BrewingMethod>(initialMethod || brewingMethods[0])
+	const initialTechnique =
+		localStorage && localStorage.getItem("selectedTechnique") ? selectedMethod.techniques.find(t => t.id === localStorage.getItem("selectedTechnique")) : selectedMethod.techniques[0]
+	const [selectedTechnique, setSelectedTechnique] = useState<BrewingTechnique>(initialTechnique || selectedMethod.techniques[0])
+	const [coffeeAmount, setCoffeeAmount] = useState(selectedTechnique.defaultCoffeeAmount)
+	const [waterAmount, setWaterAmount] = useState(selectedTechnique.ratio * selectedTechnique.defaultCoffeeAmount)
 
-  const handleMethodChange = (method: BrewingMethod) => {
-    setSelectedMethod(method);
-    const newTechnique = method.techniques[0];
-    setSelectedTechnique(newTechnique);
-    setCoffeeAmount(newTechnique.defaultCoffeeAmount);
-    setWaterAmount(newTechnique.ratio * newTechnique.defaultCoffeeAmount);
-  };
+	const handleMethodChange = (method: BrewingMethod) => {
+		setSelectedMethod(method)
+		localStorage.setItem("selectedMethod", method.id)
+		const newTechnique = method.techniques[0]
+		localStorage.setItem("selectedTechnique", newTechnique.id)
+		setSelectedTechnique(newTechnique)
+		setCoffeeAmount(newTechnique.defaultCoffeeAmount)
+		setWaterAmount(newTechnique.ratio * newTechnique.defaultCoffeeAmount)
+	}
 
-  const handleTechniqueChange = (technique: BrewingTechnique) => {
-    setSelectedTechnique(technique);
-    setCoffeeAmount(technique.defaultCoffeeAmount);
-    setWaterAmount(technique.ratio * technique.defaultCoffeeAmount);
-  };
+	const handleTechniqueChange = (technique: BrewingTechnique) => {
+		localStorage.setItem("selectedTechnique", technique.id)
+		setSelectedTechnique(technique)
+		setCoffeeAmount(technique.defaultCoffeeAmount)
+		setWaterAmount(technique.ratio * technique.defaultCoffeeAmount)
+	}
 
-  return (
-    <div className="min-h-screen bg-background p-8 pb-2 antialiased flex flex-col justify-between">
-      <div>
-        <h1 className="text-2xl mb-16 text-center tracking-tight items-center flex justify-center gap-2">
-          <Coffee strokeWidth={1.5} /> webrew
-        </h1>
-        <div className="max-w-xl mx-auto space-y-8">
-          <MethodSelection
-            methods={brewingMethods}
-            selectedMethod={selectedMethod}
-            selectedTechnique={selectedTechnique}
-            onSelectMethod={handleMethodChange}
-            onSelectTechnique={handleTechniqueChange}
-          />
-          <CoffeeAmountInput
-            coffeeAmount={coffeeAmount}
-            setCoffeeAmount={setCoffeeAmount}
-            waterAmount={waterAmount}
-            setWaterAmount={setWaterAmount}
-            ratio={selectedTechnique.ratio}
-          />
-          <BrewingGuide
-            technique={selectedTechnique}
-            coffeeAmount={coffeeAmount}
-          />
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="min-h-screen bg-background p-8 pb-2 antialiased flex flex-col justify-between">
+			<div>
+				<h1 className="text-2xl mb-16 text-center tracking-tight items-center flex justify-center gap-2">
+					<Coffee strokeWidth={1.5} /> webrew
+				</h1>
+				<div className="max-w-xl mx-auto space-y-8">
+					<MethodSelection
+						methods={brewingMethods}
+						selectedMethod={selectedMethod}
+						selectedTechnique={selectedTechnique}
+						onSelectMethod={handleMethodChange}
+						onSelectTechnique={handleTechniqueChange}
+					/>
+					<CoffeeAmountInput coffeeAmount={coffeeAmount} setCoffeeAmount={setCoffeeAmount} waterAmount={waterAmount} setWaterAmount={setWaterAmount} ratio={selectedTechnique.ratio} />
+					<BrewingGuide technique={selectedTechnique} coffeeAmount={coffeeAmount} />
+				</div>
+			</div>
+			<Footer />
+		</div>
+	)
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,31 +1,21 @@
 const Footer = () => {
-  return (
-    <footer className="border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-center gap-4 md:h-12 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{" "}
-          <a
-            href="https://junaidanjum.com"
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            Junaid A.
-          </a>
-          . The source code is available on{" "}
-          <a
-            href="https://github.com/junaidanjum/webrew"
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            GitHub
-          </a>
-          .
-        </p>
-      </div>
-    </footer>
-  );
-};
+	return (
+		<footer className="border-t border-border/40 py-2 dark:border-border md:px-8 md:py-0 mt-6">
+			<div className="container flex flex-col items-center justify-center gap-4 md:h-12 md:flex-row">
+				<p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+					Built by{" "}
+					<a href="https://junaidanjum.com" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
+						Junaid A.
+					</a>
+					. The source code is available on{" "}
+					<a href="https://github.com/junaidanjum/webrew" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
+						GitHub
+					</a>
+					.
+				</p>
+			</div>
+		</footer>
+	)
+}
 
-export default Footer;
+export default Footer
